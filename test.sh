cf7ce356af61c2872aac72a14fd19b3e1ccc601c
JM Copino <jose@clicksend.com>
2022-04-18 15:34:29 +0800

    fix test.sh

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 #test for command topic
-mosquitto_pub -h 127.0.0.1 -tmqtt-blinds/bed_4_curtain/set -m 'close'
+mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_4_curtain/set -m 'close'
 
 #test for set position topic
 mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_4_curtain/position/set -m '40'
