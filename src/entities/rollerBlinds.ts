e93279b7cf2d652315135cc476be1b1299e81fd2
JM Copino <jose@clicksend.com>
2022-04-22 14:01:52 +0800

    testing docker

--- src/entities/rollerBlinds.ts
+++ src/entities/rollerBlinds.ts
@@ -51,7 +51,6 @@ const connect = (
   const write = (data: string, cb?: (error?: Error) => void) => {
     logger.info(`TCP command to be sent-: ${data}`);
     client.write(data);
-    console.log("DONE WRITE");
   };
 
   return {
