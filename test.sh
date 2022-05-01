ce0dfd0a94b6a8adf0ae0ea28765aafb2ce70056
JM Copino <jose@clicksend.com>
2022-05-01 12:07:42 +0800

    development: test.sh

--- test.sh
+++ test.sh
@@ -2,17 +2,17 @@
 
 # UDP Protocol
 #test for command topic
-mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_1_roller_blind/set -m 'opn'
+mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_1_curtain/set -m 'opn'
 
 #test for set position topic
-mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_1_roller_blind/position/set -m '50'
+mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_1_curtain/position/set -m '50'
 
 
 # TCP Protocol
 #test for command topic
-mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_4_curtain/set -m 'close'
+mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/kitchen_roller_blind/set -m 'close'
 
 #test for set position topic
-mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/bed_4_curtain/position/set -m '40'
+mosquitto_pub -h 127.0.0.1 -t mqtt-blinds/kitchen_roller_blind/position/set -m '40'
 
 
