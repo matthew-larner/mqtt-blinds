f0e4391b371d4a443b9f38be1b22aa94b2ef642b
JM Copino <jose@clicksend.com>
2022-05-06 17:22:41 +0800

    rollerBlinds revert

--- src/entities/rollerBlinds.ts
+++ src/entities/rollerBlinds.ts
@@ -1,56 +1,30 @@
 import * as net from "net";
 import * as logger from "../lib/logger/logger";
 
-const connect = (
-  host: string,
-  port: number,
-  reconnectSeconds: number = 15,
-  idleSeconds: number = 60
-) => {
+const connect = async (host: string, port: number) => {
   const client = new net.Socket();
-  let timedOut = false;
 
   client.on("connect", () => {
-    logger.info("Connected to roller-blind");
-    client.setTimeout(idleSeconds * 1000);
+    logger.info("Connected to TCP/UDP server");
   });
 
   client.on("close", () => {
-    logger.info("roller-blind connection closed");
-
-    if (timedOut) {
-      client.connect(port, host);
-      timedOut = false;
-
-      return;
-    }
-
-    setTimeout(() => {
-      client.connect(port, host);
-    }, reconnectSeconds * 1000);
+    logger.info("TCP/UDP connection closed");
   });
 
   client.on("error", (err) => {
     logger.info(`Blind error: ${err.message}`);
   });
 
-  client.on("timeout", () => {
-    logger.info(
-      `No TCP communication detected in the last ${idleSeconds} seconds. Force reconnecting...`
-    );
-    timedOut = true;
-    client.end();
-  });
-
   client.connect(port, host);
 
-  const onMessage = (callback: (data: Buffer) => void) => {
-    client.on("data", callback);
+  const onMessage = async (callback: (data: Buffer) => void) => {
+    await client.on("data", callback);
   };
 
   const write = (data: string, cb?: (error?: Error) => void) => {
     logger.info(`TCP command to be sent-: ${data}`);
-    client.write(data);
+    return client.write(data);
   };
 
   return {
